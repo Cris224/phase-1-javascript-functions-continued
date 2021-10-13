ebf32fd3659f88a40724e0dac11c487c608c13da
Cristofer <cristoferlorenzana@gmail.com>
2021-10-12 21:23:18 -0500

    commit tree 253fbbf6f40cb347c6f1b1f74757e4d311029919

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(string = 'roller-skate'){
+    return `This Saturday, I want to ${string}!`;
+}
+
+function mondayWork(strung = 'go to the office'){
+    return `This Monday, I will ${strung}.`
+}
+
+function wrapAdjective(detail = '*'){
+    return function inner(defaul = 'string'){ 
+        return `You are ${detail}${defaul}${detail}!` 
+    }
+}
